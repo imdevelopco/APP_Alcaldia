e4c3e0d494612bfb4b3fd2b149e4a509b74f8228
dev <33762937+imdevelopco@users.noreply.github.com>
2019-08-15 12:10:19 -0500

    Primer Push: Tabla hecha en React-data-grid

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Grid_alcaldia from './Grid';
+import styles from'./Grid.css'
+
+function App() {
+  return <div > <Grid_alcaldia className={styles.Grilla_Datos}/></div>;
+}
+
+export default App;
---
+++ src/Grid.js
@@ -0,0 +1,42 @@
+import React,{Component} from 'react';
+import ReactDataGrid from 'react-data-grid';
+
+const columns = [
+  { key: "id", name: "ID", editable: true },
+  { key: "title", name: "Title", editable: true },
+  { key: "complete", name: "Complete", editable: false }
+];
+
+const rows = new Array();
+/* Llenar tabla con Datos*/
+
+for (var i = 0; i < 100;i++) {
+    rows.push({ id: i,   title: "Task 1", complete: i+10 });
+}
+
+class Grid_alcaldia extends Component {
+  state = { rows };
+    onGridRowsUpdated = ({ fromRow, toRow, updated }) => {
+      this.setState(state => {
+        const rows = state.rows.slice();
+        for (let i = fromRow; i <= toRow; i++) {
+          // Aqui se Actualiza
+          rows[i] = { ...rows[i], ...updated };
+        }
+        return { rows };
+      });
+    };
+  render(){
+    return(<div>
+      <ReactDataGrid
+        columns={columns}
+        rowGetter={i => this.state.rows[i]}
+        rowsCount={100}
+        onGridRowsUpdated={this.onGridRowsUpdated}
+        enableCellSelect={true}
+      /> </div>
+  );
+  }
+}
+
+export default Grid_alcaldia;
